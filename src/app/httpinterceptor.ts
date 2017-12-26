73686500bc94c41790f8b3d9d77ed5e7c3ec2d4f
Josué Camelo <josueprg@gmail.com>
2017-12-25 22:02:45 -0200

    Colocando um Interceptador de Requisiçoes Http

--- src/app/httpinterceptor.ts
+++ src/app/httpinterceptor.ts
@@ -9,18 +9,40 @@ import 'rxjs/add/operator/catch';
 export class MyHttpInterceptor implements HttpInterceptor {
     constructor() { }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    //metodo de teste não modificar
+    /*intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("intercepted request ... ");
-// Clone the request to add the new header.
+        // Clone the request to add the new header.
         const authReq = req.clone({ headers: req.headers.set("headerName", "headerValue")});
         console.log("Sending request with new header now ...");
-//send the newly created request
+        //send the newly created request
         return next.handle(authReq)
             .catch((error, caught) => {
-//intercept the respons error and displace it to the console
+                //intercept the respons error and displace it to the console
                 console.log("Error Occurred");
                 console.log(error);
-//return the error to the method that called it
+                console.log(error.status); // --> 404
+                console.log(caught);
+                //return the error to the method that called it
+                return Observable.throw(error);
+            }) as any;
+    }*/
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log("interceptando requisição ... ");
+        // Clone the request to add the new header.
+        const authReq = req.clone({ headers: req.headers.set("headerName", "headerValue")});
+        console.log("Enviando requisição com Novos Headers ...");
+        //send the newly created request
+        return next.handle(authReq)
+            .catch((error, caught) => {
+                //intercept the respons error and displace it to the console
+                console.log("Ops Ocorreu um Erro");
+                console.log(error);
+                console.log(error.statusText);
+                console.log(error.status); // --> 404
+                console.log(caught);
+                //return the error to the method that called it
                 return Observable.throw(error);
             }) as any;
     }
